3c1e2b80275aaba95e65ea03438557ce5f215b68
Tadeusz Ciszewski <office@tc-soft.pl>
2021-07-09 19:38:30 +0200

    feat: Add DatabaseOptions class to read appsettings.json section

---
+++ mini-ITS.Core/Options/DatabaseOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace mini_ITS.Core.Options
+{
+    public class DatabaseOptions
+    {
+        public string DataSource { get; set; }
+        public string InitialCatalog { get; set; }
+        public bool PersistSecurityInfo { get; set; }
+        public bool IntegratedSecurity { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public int ConnectTimeout { get; set; }
+    }
+}

cdf180dd5593f5e5b2d6e9e44a761445175157ac
Tadeusz Ciszewski <office@tc-soft.pl>
2021-07-11 11:09:08 +0200

    feat: Add Constants class

---
+++ mini-ITS.Core/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace mini_ITS.Core
+{
+    public static class Constants
+    {
+        public const string DATE_FORMAT = "dd.MM.yyyy";
+
+        public const string DATE_FORMAT_STRING = "{0:dd.MM.yyyy}";
+
+        public const string DATE_TIME_FORMAT = "dd.MM.yyyy HH:mm";
+
+        public const string DATE_TIME_FORMAT_LOG = "yyyyMMddHHmm";
+
+        public const string DATE_TIME_FORMAT_STRING = "{0:dd.MM.yyyy HH:mm}";
+
+        public const string TIME_LONG_FORMAT = "HH:mm:ss.fff";
+    }
+}

c991d7c071658eaa503a041c7209ef58a7795911
Tadeusz Ciszewski <office@tc-soft.pl>
2021-07-11 11:18:56 +0200

    feat: Add SqlQueryCondition class

---
+++ mini-ITS.Core/Database/SqlQueryCondition.cs
@@ -0,0 +1,9 @@
+﻿namespace mini_ITS.Core.Database
+{
+    public class SqlQueryCondition
+    {
+        public string Name { get; set; }
+        public string Operator { get; set; }
+        public object Value { get; set; }
+    }
+}

52997ba20e1b200f5222523dcb42fd6e82409e4f
Tadeusz Ciszewski <office@tc-soft.pl>
2021-07-25 16:41:55 +0200

    feat: Add UsersServices class and add DI do Startup.cs

---
+++ mini-ITS.Core/Services/UsersServices.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using mini_ITS.Core.Models;
+using mini_ITS.Core.Repository;
+
+namespace mini_ITS.Core.Services
+{
+    public class UsersServices : IUsersServices
+    {
+        private readonly IUsersRepository _usersRepository;
+        private readonly IMapper _mapper;
+        private readonly IPasswordHasher<Users> _passwordHasher;
+
+        public UsersServices(IUsersRepository usersRepository, IMapper mapper, IPasswordHasher<Users> passwordHasher)
+        {
+            _usersRepository = usersRepository;
+            _mapper = mapper;
+            _passwordHasher = passwordHasher;
+        }
+    }
+}

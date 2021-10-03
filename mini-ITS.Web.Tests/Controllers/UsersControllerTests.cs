573894dee426009d037a19b8633329bbbb6a6afc
Tadeusz Ciszewski <office@tc-soft.pl>
2021-10-03 21:16:30 +0200

    feat: Add UsersController + mini-ITS.Web.Tests project

---
+++ mini-ITS.Web.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+
+namespace mini_ITS.Web.Tests.Controllers
+{
+    [TestFixture]
+    class UsersControllerTests
+    {
+    }
+}
---
+++ mini-ITS.Web/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mini_ITS.Core.Services;
+
+namespace mini_ITS.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersServices _usersServices;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UsersController(IUsersServices usersServices, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            _usersServices = usersServices;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}

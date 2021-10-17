faa9c2cc3683e9955d5aa15d32397b2abe623fef
Tadeusz Ciszewski <office@tc-soft.pl>
2021-10-17 14:59:41 +0200

    feat: Add ApiRoutes class, for defined endpoints to use by tests

---
+++ mini-ITS.Web.Tests/ApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace mini_ITS.Web.Tests
+{
+    public static class ApiRoutes
+    {
+        private static readonly string _baseUrl = "https://localhost:44375/api/";
+
+        public static class Users
+        {
+            private static readonly string _usersControllerUrl = string.Concat(_baseUrl, "Users");
+
+            public static readonly string Login = string.Concat(_usersControllerUrl, "/Login");
+        }
+    }
+}

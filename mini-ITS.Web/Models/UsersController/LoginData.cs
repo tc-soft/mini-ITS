e05d2e8d53dd56620ce550e34ecc20331ab31744
Tadeusz Ciszewski <office@tc-soft.pl>
2021-10-17 13:53:30 +0200

    feat: Add LoginData class, for modeling login process

---
+++ mini-ITS.Web/Models/UsersController/LoginData.cs
@@ -0,0 +1,8 @@
+﻿namespace mini_ITS.Web.Models.UsersController
+{
+    public class LoginData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}

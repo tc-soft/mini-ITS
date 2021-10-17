ef1e7887ce8e0750bdc1943d5952b7d6ec6edcca
Tadeusz Ciszewski <office@tc-soft.pl>
2021-10-17 14:49:52 +0200

    feat: Add LoginJsonResults class, for modeling results of login process

---
+++ mini-ITS.Web/Models/UsersController/LoginJsonResults.cs
@@ -0,0 +1,12 @@
+﻿namespace mini_ITS.Web.Models.UsersController
+{
+    public class LoginJsonResults
+    {
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+        public bool isLogged { get; set; }
+    }
+}

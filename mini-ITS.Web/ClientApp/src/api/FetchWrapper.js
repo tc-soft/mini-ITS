8700e7d0d70032e382a42bb570dcd629620123b0
Tadeusz Ciszewski <office@tc-soft.pl>
2021-11-14 17:02:17 +0100

    feat Front-End: Add to src/api/FetchWrapper.js login function

---
+++ mini-ITS.Web/ClientApp/src/api/FetchWrapper.js
@@ -0,0 +1,19 @@
+﻿export const fetchWrapper = {
+    login
+}
+
+function login(url, login, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            login: login,
+            password: password
+        })
+    };
+
+    return fetch(url, requestOptions);
+}

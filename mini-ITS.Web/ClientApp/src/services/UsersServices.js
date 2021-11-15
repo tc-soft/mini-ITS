bae9846e2b223b83c546ef647eadec1f0ef345a8
Tadeusz Ciszewski <office@tc-soft.pl>
2021-11-15 21:26:53 +0100

    feat Front-End: Add to src/services/UsersServices.js login function

---
+++ mini-ITS.Web/ClientApp/src/services/UsersServices.js
@@ -0,0 +1,11 @@
+﻿import { fetchWrapper } from '../api/FetchWrapper';
+
+export const usersServices = {
+    login
+}
+
+const baseUrl = 'api/Users';
+
+function login(login, password) {
+    return fetchWrapper.login(`${baseUrl}/Login`, login, password);
+}

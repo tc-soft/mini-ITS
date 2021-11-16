1ed0d6a90ec01263459f6c02a7b9a2a9a1f21217
Tadeusz Ciszewski <office@tc-soft.pl>
2021-11-16 18:40:00 +0100

    feat Front-End: Add src/components/AuthProvider.js - global AuthProvider

---
+++ mini-ITS.Web/ClientApp/src/components/AuthProvider.js
@@ -0,0 +1,73 @@
+﻿import { createContext, useContext, useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { usersServices } from '../services/UsersServices';
+
+const AuthContext = createContext();
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+export default function AuthProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [loginStatus, setLoginStatus] = useState(false);
+    const navigate = useNavigate();
+
+    if (!currentUser && !loginStatus) {
+        try {
+            usersServices.loginStatus()
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json()
+                            .then((data) => {
+                                console.log('currentUser : check login status');
+                                setCurrentUser(data);
+                                setLoginStatus(true);
+                            })
+                    }
+                });
+        }
+        catch (error) {
+            console.log('Error while loginStatus', error.message);
+        }
+    };
+
+    const handleLogin = (user) => {
+        try {
+            setCurrentUser(user);
+        }
+        catch (error) {
+            console.log('Error while logging', error.message);
+        }
+    };
+
+    const handleLogout = () => {
+        try {
+            usersServices.logout()
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json()
+                            .then((data) => {
+                                setCurrentUser(null);
+                                navigate('/');
+                                console.info(data);
+                            })
+                    } else {
+                        return response.json()
+                            .then((data) => {
+                                console.warn(data);
+                            })
+                    }
+                });
+        }
+        catch (error) {
+            console.log('Error while logging out', error.message);
+        }
+    };
+
+    return (
+        <AuthContext.Provider value={{ currentUser, handleLogin, handleLogout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}

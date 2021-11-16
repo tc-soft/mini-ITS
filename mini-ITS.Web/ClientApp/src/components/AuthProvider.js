cd056662a3cfa62b30bd99e60013882f511d18d2
Tadeusz Ciszewski <office@tc-soft.pl>
2021-11-16 19:03:52 +0100

    feat Front-End: Mod AuthProvider.js encoding to Unicode UTF-8 without signature

--- mini-ITS.Web/ClientApp/src/components/AuthProvider.js
+++ mini-ITS.Web/ClientApp/src/components/AuthProvider.js
@@ -1,4 +1,4 @@
-﻿import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { usersServices } from '../services/UsersServices';
 

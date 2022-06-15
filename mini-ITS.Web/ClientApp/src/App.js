4151e6f4c5eee30be6a68b4e35c2ccdc5f1ad77c
Tadeusz Ciszewski <office@tc-soft.pl>
2022-06-15 18:06:31 +0200

    feat Front-End: Add src/pages/login/RequireAuth.js

--- mini-ITS.Web/ClientApp/src/App.js
+++ mini-ITS.Web/ClientApp/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { useAuth } from './components/AuthProvider';
+import Login from './pages/login/Login';
+import RequireAuth from './pages/login/RequireAuth';
 import { ReactComponent as BrandIcon } from "./images/mini-ITS.svg";
 import { ReactComponent as LogOut } from "./images/LogOut.svg";
 
@@ -14,18 +16,22 @@ function App() {
             <header className="main__header">
                 <nav>
                     <BrandIcon />
-
                     <ul>
-                        <li className="main__header--link">{<Link to="/">Home</Link>}</li>
+                        <li className="main__header--link">{currentUser && <Link to="/">Home</Link>}</li>
+                        <li className="main__header--link">{currentUser && <Link to="/Test">Użytkownicy</Link>}</li>
                         <li className="main__header--icon">{currentUser && <Link to='/' onClick={() => { handleLogout() }}><LogOut /></Link>}</li>
                     </ul>
-
                 </nav>
             </header>
 
             <section className="main__section">
                 <Routes>
-                    <Route path="/" element={<p>Strona główna</p>}/>
+                    <Route path="/" element={
+                        <RequireAuth>
+                            <p>Strona główna</p>
+                        </RequireAuth>}
+                    />
+                    <Route path="/Login" element={<Login />}/>
                 </Routes>
             </section>
 

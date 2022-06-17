20d2354e8f72d592147d5c5543e2a66b9f15035a
Tadeusz Ciszewski <office@tc-soft.pl>
2022-06-17 18:52:43 +0200

    feat Front-End: Upgrade to React 18

--- mini-ITS.Web/ClientApp/src/index.js
+++ mini-ITS.Web/ClientApp/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from './components/AuthProvider';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
                 <App />
             </AuthProvider>
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

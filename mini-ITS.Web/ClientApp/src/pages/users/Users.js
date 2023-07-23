abd18b89594c220e777ca923f0692d5d5f07685c
Tadeusz Ciszewski <office@tc-soft.pl>
2023-07-23 14:35:23 +0200

    feat Front-End: Add loading data to src/pages/users/UsersList.js

--- mini-ITS.Web/ClientApp/src/pages/users/Users.js
+++ mini-ITS.Web/ClientApp/src/pages/users/Users.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import UsersList from './UsersList';
 
 const Users = () => {
+    const [pagedQuery, setPagedQuery] = useState({
+        filter: null,
+        sortColumnName: 'Login',
+        sortDirection: 'ASC',
+        page: 1,
+        resultsPerPage: 10
+    });
+    const [activeDepartmentFilter, setActiveDepartmentFilter] = useState('');
+    const [activeRoleFilter, setActiveRoleFilter] = useState('');
 
     return (
         <Routes>
-            <Route path="/" element={<UsersList />} />
+            <Route index element={<UsersList
+                pagedQuery={pagedQuery}
+                setPagedQuery={setPagedQuery}
+                activeDepartmentFilter={activeDepartmentFilter}
+                setActiveDepartmentFilter={setActiveDepartmentFilter}
+                activeRoleFilter={activeRoleFilter}
+                setActiveRoleFilter={setActiveRoleFilter}
+                />}
+            />
         </Routes>
     );
 };
--- mini-ITS.Web/ClientApp/src/pages/users/UsersList.js
+++ mini-ITS.Web/ClientApp/src/pages/users/UsersList.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { usersServices } from '../../services/UsersServices';
 
-const UsersList = () => {
+const UsersList = (props) => {
+    const {
+        pagedQuery,
+        activeDepartmentFilter,
+        activeRoleFilter
+    } = props;
+
+    const [users, setUsers] = useState({
+        results: null,
+        page: null,
+        resultsPerPage: null,
+        totalResults: null,
+        totalPages: null
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await usersServices.index(pagedQuery);
+                if (response.ok) {
+                    const data = await response.json();
+                    setUsers(data);
+                }
+                else {
+                    throw new Error('Network response was not ok');
+                };
+            }
+            catch (error) {
+                console.error('Error loading data:', error);
+            };
+        };
+
+        setTimeout(fetchData, 0);
+    }, [pagedQuery, activeDepartmentFilter, activeRoleFilter]);
 
     return (
         <>
@@ -19,15 +53,29 @@ const UsersList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    {users.results && users.results.map((user, index) => {
+                        const record = index + ((users.page - 1) * users.resultsPerPage) + 1;
+                        return (
+                            <tr key={index}>
+                                <td>{record}</td>
+                                <td>{user.login}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.department}</td>
+                                <td>{user.role}</td>
+                                <td></td>
+                            </tr>
+                        );
+                    }
+                    )}
+                    {
+                        users.results === null &&
+                        <tr className='usersList-table--info'>
+                            <td colSpan='7'>
+                                <div>Brak danych...</div>
+                            </td>
+                        </tr>
+                    }
                 </tbody>
             </table>
         </>

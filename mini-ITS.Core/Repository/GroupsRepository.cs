c16b599136c6427e8e7b0ef501f82aff677fec3b
Tadeusz Ciszewski <office@tc-soft.pl>
2023-08-03 20:51:17 +0200

    feat: Add GroupsRepository class and add DI do Startup.cs

---
+++ mini-ITS.Core/Repository/GroupsRepository.cs
@@ -0,0 +1,14 @@
+﻿using mini_ITS.Core.Database;
+
+namespace mini_ITS.Core.Repository
+{
+    public class GroupsRepository : IGroupsRepository
+    {
+        private readonly string _connectionString;
+
+        public GroupsRepository(ISqlConnectionString sqlConnectionString)
+        {
+            _connectionString = sqlConnectionString.ConnectionString;
+        }
+    }
+}

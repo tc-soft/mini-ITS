197fbd71bdde7e2b3137446202877dc1223f36b3
Tadeusz Ciszewski <office@tc-soft.pl>
2023-08-03 19:24:39 +0200

    feat: Add Groups class

---
+++ mini-ITS.Core/Models/Groups.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace mini_ITS.Core.Models
+{
+    public class Groups
+    {
+        public Guid Id { get; set; }
+        public DateTime DateAddGroup { get; set; }
+        public DateTime DateModGroup { get; set; }
+
+        public Guid UserAddGroup { get; set; }
+        public string UserAddGroupFullName { get; set; }
+        public Guid UserModGroup { get; set; }
+        public string UserModGroupFullName { get; set; }
+
+        public string GroupName { get; set; }
+
+        private Groups() { }
+
+        public Groups(
+            Guid id,
+            DateTime dateAddGroup,
+            DateTime dateModGroup,
+            Guid userAddGroup,
+            string userAddGroupFullName,
+            Guid userModGroup,
+            string userModGroupFullName,
+            string groupName)
+        {
+            Id = id;
+            DateAddGroup = dateAddGroup;
+            DateModGroup = dateModGroup;
+            UserAddGroup = userAddGroup;
+            UserAddGroupFullName = userAddGroupFullName;
+            UserModGroup = userModGroup;
+            UserModGroupFullName = userModGroupFullName;
+            GroupName = groupName;
+        }
+    }
+}

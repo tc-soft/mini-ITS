6945cc238336afd4ccbba29c3979016320b8bc77
Tadeusz Ciszewski <office@tc-soft.pl>
2023-08-06 10:50:49 +0200

    feat: Add GroupsDto class

---
+++ mini-ITS.Core/Dto/GroupsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace mini_ITS.Core.Dto
+{
+    public class GroupsDto
+    {
+        public Guid Id { get; set; }
+        public DateTime DateAddGroup { get; set; }
+        public DateTime DateModGroup { get; set; }
+
+        public Guid UserAddGroup { get; set; }
+        public string UserAddGroupFullName { get; set; }
+        public Guid UserModGroup { get; set; }
+        public string UserModGroupFullName { get; set; }
+
+        public string GroupName { get; set; }
+    }
+}

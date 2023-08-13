e904271e539257b834da0860d28c82d13bd0903d
Tadeusz Ciszewski <office@tc-soft.pl>
2023-08-13 18:45:27 +0200

    feat: Add GroupsController + GroupsControllerTests class

---
+++ mini-ITS.Web.Tests/GroupsIntegrationTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace mini_ITS.Web.Tests
+{
+    public class GroupsIntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        protected HttpResponseMessage response;
+
+        protected GroupsIntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
---
+++ mini-ITS.Web/Controllers/GroupsController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using mini_ITS.Core.Services;
+
+namespace mini_ITS.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class GroupsController : ControllerBase
+    {
+        private readonly IGroupsServices _groupsServices;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public GroupsController(IGroupsServices groupsServices, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            _groupsServices = groupsServices;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+    }
+}

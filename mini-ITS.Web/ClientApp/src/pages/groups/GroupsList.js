68a727022fd5cfbd7d16f19e465073f319d09652
Tadeusz Ciszewski <office@tc-soft.pl>
2023-08-25 21:19:31 +0200

    feat Front-End: Add src/pages/groups/GroupsList.js

---
+++ mini-ITS.Web/ClientApp/src/pages/groups/GroupsList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const GroupsList = () => {
+
+    return (
+        <>
+            <p>Lista grup</p>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{ width: '05%' }}>Lp.</th>
+                        <th style={{ width: '80%' }}>Nazwa grupy</th>
+                        <th style={{ width: '15%' }}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default GroupsList;

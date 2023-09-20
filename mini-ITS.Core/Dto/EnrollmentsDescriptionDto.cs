bb963f365f112400d971512dc5f8377fcdc55526
Tadeusz Ciszewski <office@tc-soft.pl>
2023-09-20 14:22:27 +0200

    feat: Add EnrollmentsDescriptionDto class

---
+++ mini-ITS.Core/Dto/EnrollmentsDescriptionDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace mini_ITS.Core.Dto
+{
+    public class EnrollmentsDescriptionDto
+    {
+        public Guid Id { get; set; }
+        public Guid EnrollmentId { get; set; }
+        public DateTime DateAddDescription { get; set; }
+        public DateTime DateModDescription { get; set; }
+
+        public Guid UserAddDescription { get; set; }
+        public string UserAddDescriptionFullName { get; set; }
+        public Guid UserModDescription { get; set; }
+        public string UserModDescriptionFullName { get; set; }
+
+        public string Description { get; set; }
+        public int ActionExecuted { get; set; }
+    }
+}

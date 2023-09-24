66eda05b8987acad12576ccf1f0825f106afc0d4
Tadeusz Ciszewski <office@tc-soft.pl>
2023-09-24 17:16:40 +0200

    feat: Add EnrollmentsPictureDto class

---
+++ mini-ITS.Core/Dto/EnrollmentsPictureDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace mini_ITS.Core.Dto
+{
+    public class EnrollmentsPictureDto
+    {
+        public Guid Id { get; set; }
+        public Guid EnrollmentId { get; set; }
+        public DateTime DateAddPicture { get; set; }
+        public DateTime DateModPicture { get; set; }
+
+        public Guid UserAddPicture { get; set; }
+        public string UserAddPictureFullName { get; set; }
+        public Guid UserModPicture { get; set; }
+        public string UserModPictureFullName { get; set; }
+
+        public string PictureName { get; set; }
+        public string PicturePath { get; set; }
+        public string PictureFullPath { get; set; }
+    }
+}

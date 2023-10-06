c1d908941c3c26c71f105a9ea6916cd6bc1cf3ca
Tadeusz Ciszewski <office@tc-soft.pl>
2023-10-06 20:59:11 +0200

    feat: Add EnrollmentsRepository class and add DI do Startup.cs

---
+++ mini-ITS.Core/Repository/EnrollmentsRepository.cs
@@ -0,0 +1,14 @@
+﻿using mini_ITS.Core.Database;
+
+namespace mini_ITS.Core.Repository
+{
+    public class EnrollmentsRepository : IEnrollmentsRepository
+    {
+        private readonly string _connectionString;
+
+        public EnrollmentsRepository(ISqlConnectionString sqlConnectionString)
+        {
+            _connectionString = sqlConnectionString.ConnectionString;
+        }
+    }
+}

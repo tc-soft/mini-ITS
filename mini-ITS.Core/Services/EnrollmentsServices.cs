c0db5f9318509842aba085f3a99791a8e7d3fcbb
Tadeusz Ciszewski <office@tc-soft.pl>
2023-10-09 20:20:46 +0200

    feat: Add EnrollmentsServices class and add DI do Startup.cs

---
+++ mini-ITS.Core/Services/EnrollmentsServices.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using mini_ITS.Core.Repository;
+
+namespace mini_ITS.Core.Services
+{
+    public class EnrollmentsServices : IEnrollmentsServices
+    {
+        private readonly IEnrollmentsRepository _enrollmentsRepository;
+        private readonly IUsersRepository _usersRepository;
+        private readonly IMapper _mapper;
+
+        public EnrollmentsServices(
+            IEnrollmentsRepository enrollmentsRepository,
+            IUsersRepository usersRepository,
+            IMapper mapper)
+        {
+            _enrollmentsRepository = enrollmentsRepository;
+            _usersRepository = usersRepository;
+            _mapper = mapper;
+        }
+    }
+}

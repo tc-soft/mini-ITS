9f796f30ff7aa5f4fbca624e29c77302f01930f7
Tadeusz Ciszewski <office@tc-soft.pl>
2024-02-18 11:57:05 +0100

    feat Front-End: Add src/pages/enrollments/EnrollmentsList.jsx

---
+++ mini-ITS.Web/ClientApp/src/pages/enrollments/EnrollmentsList.jsx
@@ -0,0 +1,39 @@
+const EnrollmentsList = () => {
+    return (
+        <>
+            <p>Lista zgłoszeń</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{ width: '07%' }}>Nr</th>
+                        <th style={{ width: '07%' }}>Data wpr.</th>
+                        <th style={{ width: '07%' }}>Data zak.</th>
+                        <th style={{ width: '15%' }}>Opis</th>
+                        <th style={{ width: '10%' }}>Zgłaszający</th>
+                        <th style={{ width: '10%' }}>Dział docelowy</th>
+                        <th style={{ width: '07%' }}>Priorytet</th>
+                        <th style={{ width: '07%' }}>Status</th>
+                        <th style={{ width: '05%' }}>Gotowe</th>
+                        <th style={{ width: '25%' }}>Operacje</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default EnrollmentsList;

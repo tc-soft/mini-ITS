38e88d3d27bf0d0893bbf7e31faeb3351db8de7d
Tadeusz Ciszewski <office@tc-soft.pl>
2024-02-18 11:13:38 +0100

    feat Front-End: Add src/pages/enrollments/Enrollments.jsx

---
+++ mini-ITS.Web/ClientApp/src/pages/enrollments/Enrollments.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+
+const Enrollments = () => {
+
+    return (
+        <Routes>
+            <Route path="/" element={<p>Zgłoszenia</p>} />
+        </Routes>
+    );
+};
+
+export default Enrollments;

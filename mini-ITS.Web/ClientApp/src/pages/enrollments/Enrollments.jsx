1c963f7a3110d1151648e79d41d70a6455762885
Tadeusz Ciszewski <office@tc-soft.pl>
2024-02-18 18:39:32 +0100

    feat Front-End: Add loading data to src/pages/enrollments/EnrollmentsList.jsx

--- mini-ITS.Web/ClientApp/src/pages/enrollments/Enrollments.jsx
+++ mini-ITS.Web/ClientApp/src/pages/enrollments/Enrollments.jsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import EnrollmentsList from './EnrollmentsList';
 
 const Enrollments = () => {
+    const [pagedQuery, setPagedQuery] = useState({
+        filter: null,
+        sortColumnName: 'DateAddEnrollment',
+        sortDirection: 'DESC',
+        page: 1,
+        resultsPerPage: 10
+    });
 
     return (
         <Routes>
             <Route path="/" element={<EnrollmentsList />} />
+            <Route index element={<EnrollmentsList
+                pagedQuery={pagedQuery}
+                setPagedQuery={setPagedQuery}
+            />}
+            />
         </Routes>
     );
 };
--- mini-ITS.Web/ClientApp/src/pages/enrollments/EnrollmentsList.jsx
+++ mini-ITS.Web/ClientApp/src/pages/enrollments/EnrollmentsList.jsx
@@ -1,4 +1,54 @@
-const EnrollmentsList = () => {
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import { enrollmentServices } from '../../services/EnrollmentServices';
+
+const EnrollmentsList = (props) => {
+    const {
+        pagedQuery
+    } = props;
+
+    const [enrollments, setEnrollments] = useState({
+        results: null,
+        page: null,
+        resultsPerPage: null,
+        totalResults: null,
+        totalPages: null
+    });
+
+    const mapPriority = {
+        '0': 'Normalny',
+        '1': 'Wysoki',
+        '2': 'Krytyczny'
+    };
+
+    const mapState = {
+        '': 'Wszystkie',
+        'New': 'Nowy',
+        'Assigned': 'W trakcie',
+        'Closed': 'Zamknięte',
+        'ReOpened': 'Otwarte ponownie'
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await enrollmentServices.index(pagedQuery);
+                if (response.ok) {
+                    const data = await response.json();
+                    setEnrollments(data);
+                }
+                else {
+                    throw new Error('Network response was not ok');
+                };
+            }
+            catch (error) {
+                console.error('Error loading data:', error);
+            };
+        };
+
+        setTimeout(fetchData, 0);
+    }, [pagedQuery]);
+
     return (
         <>
             <p>Lista zgłoszeń</p>
@@ -18,18 +68,50 @@ const EnrollmentsList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    {enrollments.results && enrollments.results.map((enrollment, index) => {
+                        const dateaddenrollment = enrollment.dateAddEnrollment ? format(new Date(enrollment.dateAddEnrollment), 'dd.MM.yyyy') : '';
+                        const dateendenrollment = enrollment.dateEndEnrollment ? format(new Date(enrollment.dateEndEnrollment), 'dd.MM.yyyy') : '';
+                        return (
+                            <tr key={index} style={{
+                                backgroundColor: enrollment.priority === 2 ? 'lavenderblush' : 'inherit'
+                            }}>
+                                <td>{enrollment.nr}/{enrollment.year}</td>
+                                <td>{dateaddenrollment}</td>
+                                <td>{dateendenrollment}</td>
+                                <td>{enrollment.description}</td>
+                                <td>{enrollment.userAddEnrollmentFullName}</td>
+                                <td>{enrollment.department}</td>
+                                <td>{mapPriority[enrollment.priority] || enrollment.priority}</td>
+                                <td>{mapState[enrollment.state] || enrollment.state}</td>
+
+                                <td
+                                    style={{
+                                        position: 'relative',
+                                        background:
+                                            enrollment.actionRequest !== 0
+                                                ? 'repeating-linear-gradient(-45deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3) 1px, transparent 2px, transparent 2px, rgba(0, 0, 0, 0.3) 3px)'
+                                                : 'inherit'
+                                    }}
+                                >
+                                    <input
+                                        type='checkbox'
+                                        checked={enrollment.readyForClose}
+                                        readOnly
+                                    />
+                                </td>
+                                <td></td>
+                            </tr>
+                        );
+                    }
+                    )}
+                    {
+                        enrollments.results === null &&
+                        <tr>
+                            <td colSpan='10'>
+                                <div>Brak danych...</div>
+                            </td>
+                        </tr>
+                    }
                 </tbody>
             </table>
         </>

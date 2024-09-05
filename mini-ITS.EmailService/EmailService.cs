a5c7e0a57aadf8a49417f6127d60d38f0a35fc39
Tadeusz Ciszewski <office@tc-soft.pl>
2024-09-05 21:56:48 +0200

    feat: Add EmailService method - SendEmailAsync(string toEmail, string subject, string message)

--- mini-ITS.EmailService/EmailService.cs
+++ mini-ITS.EmailService/EmailService.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace mini_ITS.EmailService
 {
@@ -9,6 +13,42 @@ namespace mini_ITS.EmailService
         public EmailService(IOptions<EmailOptions> emailOptions)
         {
             _emailOptions = emailOptions.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress(_emailOptions.FromName, _emailOptions.FromAddress));
+            emailMessage.To.Add(new MailboxAddress("", toEmail));
+            emailMessage.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder { HtmlBody = message };
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(_emailOptions.LocalDomain))
+                    {
+                        client.LocalDomain = _emailOptions.LocalDomain;
+                    }
+
+                    await client.ConnectAsync(_emailOptions.MailServerAddress, _emailOptions.MailServerPort, _emailOptions.SecureSocketOption);
+                    await client.AuthenticateAsync(_emailOptions.UserId, _emailOptions.UserPassword);
+                    await client.SendAsync(emailMessage);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("An error occurred while sending email:", ex);
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
         }
     }
 }
--- mini-ITS.EmailService/IEmailService.cs
+++ mini-ITS.EmailService/IEmailService.cs
@@ -1,6 +1,9 @@
-﻿namespace mini_ITS.EmailService
+﻿using System.Threading.Tasks;
+
+namespace mini_ITS.EmailService
 {
     public interface IEmailService
     {
+        Task SendEmailAsync(string toEmail, string subject, string message);
     }
 }

0797d396d5934fd3861f0316b04ca72a53094bed
Tadeusz Ciszewski <office@tc-soft.pl>
2024-09-08 15:46:49 +0200

    feat: Add SmsService method - SendSmsAsync(string phoneNumber, string message, string senderName)

--- mini-ITS.SmsService/ISmsService.cs
+++ mini-ITS.SmsService/ISmsService.cs
@@ -1,6 +1,9 @@
-﻿namespace mini_ITS.SmsService
+﻿using System.Threading.Tasks;
+
+namespace mini_ITS.SmsService
 {
     public interface ISmsService
     {
+        Task<string> SendSmsAsync(string phoneNumber, string message, string senderName = null);
     }
 }
--- mini-ITS.SmsService/SmsService.cs
+++ mini-ITS.SmsService/SmsService.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 
 namespace mini_ITS.SmsService
@@ -13,6 +16,45 @@ namespace mini_ITS.SmsService
         {
             _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
             _smsOptions = smsOptions?.Value ?? throw new ArgumentNullException(nameof(smsOptions));
+        }
+
+        public async Task<string> SendSmsAsync(string phoneNumber, string message, string senderName = null)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                throw new ArgumentException("Phone number cannot be empty", nameof(phoneNumber));
+
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentException("Message cannot be empty", nameof(message));
+
+            var smsData = new
+            {
+                username = _smsOptions.UserId,
+                password = _smsOptions.UserPassword,
+                system = "client_csharp",
+                details = "1",
+
+                phone = phoneNumber,
+                text = message,
+                sender = senderName ?? _smsOptions.Sender,
+            };
+
+            try
+            {
+                var content = new StringContent(JsonSerializer.Serialize(smsData), Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync($"{_smsOptions.ApiUrl}/messages/send_sms", content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException($"Error while sending SMS: {response.StatusCode}");
+                }
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"An error occurred while sending SMS: {ex.Message}", ex);
+            }
         }
     }
 }

c61b5fb425a39b66c9e4ea7b737e1b755a2d6e90
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-20 16:47:10 +0200

    feat: Add CronField class + NUnit test

---
+++ mini-ITS.SchedulerService.Tests/SchedulerTaskTestsData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace mini_ITS.SchedulerService.Tests
+{
+    public class SchedulerTaskTestsData
+    {
+        public static IEnumerable<TestCaseData> ValidCronFieldTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("*", 0, 59, Enumerable.Range(0, 60).ToArray());
+                yield return new TestCaseData("*/15", 0, 59, new int[] { 0, 15, 30, 45 });
+                yield return new TestCaseData("*/30", 0, 59, new int[] { 0, 30 });
+                yield return new TestCaseData("*/40", 0, 59, new int[] { 0, 40 });
+                yield return new TestCaseData("1-5", 0, 59, new int[] { 1, 2, 3, 4, 5 });
+                yield return new TestCaseData("45-50", 0, 59, new int[] { 45, 46, 47, 48, 49, 50 });
+                yield return new TestCaseData("1,17,45,58", 0, 59, new int[] { 1, 17, 45, 58 });
+                yield return new TestCaseData("2,13,25,48", 0, 59, new int[] { 2, 13, 25, 48 });
+            }
+        }
+        public static IEnumerable<TestCaseData> InvalidCronFieldTestCases
+        {
+            get
+            {
+                yield return new TestCaseData("60", 0, 59);
+                yield return new TestCaseData("-1", 0, 59);
+                yield return new TestCaseData("abc", 0, 59);
+                yield return new TestCaseData(null, 0, 59);
+                yield return new TestCaseData("10", 59, 0);
+                yield return new TestCaseData("50-80", 0, 59);
+                yield return new TestCaseData("1,17,45,62", 0, 59);
+                yield return new TestCaseData("71,69", 0, 59);
+            }
+        }
+    }
+}
---
+++ mini-ITS.SchedulerService/Cron/CronField.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mini_ITS.SchedulerService.Cron
+{
+    public class CronField
+    {
+        private readonly List<int> _values;
+
+        public CronField(string field, int minValue, int maxValue)
+        {
+            if (string.IsNullOrWhiteSpace(field))
+            {
+                throw new FormatException("Invalid CRON expression: field cannot be null or empty.");
+            }
+
+            if (minValue >= maxValue)
+            {
+                throw new FormatException($"Invalid CRON expression: MinValue ({minValue}) should be less than MaxValue ({maxValue}).");
+            }
+
+            _values = ParseField(field, minValue, maxValue);
+
+            if (_values.Count == 0)
+            {
+                throw new FormatException($"Invalid CRON expression: the field '{field}' does not contain any values within the range {minValue}-{maxValue}.");
+            }
+        }
+        private List<int> ParseField(string field, int minValue, int maxValue)
+        {
+            var values = new List<int>();
+
+            if (field == "*")
+            {
+                for (int i = minValue; i <= maxValue; i++)
+                {
+                    values.Add(i);
+                }
+            }
+            else if (field.Contains('/'))
+            {
+                var parts = field.Split('/');
+                int step = int.Parse(parts[1]);
+                string range = parts[0];
+
+                int start = minValue;
+                int end = maxValue;
+
+                if (range != "*")
+                {
+                    var rangeParts = range.Split('-');
+                    start = int.Parse(rangeParts[0]);
+                    end = int.Parse(rangeParts[1]);
+                }
+
+                for (int i = start; i <= end; i += step)
+                {
+                    values.Add(i);
+                }
+            }
+            else if (field.Contains(','))
+            {
+                var parts = field.Split(',');
+                foreach (var part in parts)
+                {
+                    values.AddRange(ParseField(part, minValue, maxValue));
+                }
+            }
+            else if (field.Contains('-'))
+            {
+                var parts = field.Split('-');
+                int start = int.Parse(parts[0]);
+                int end = int.Parse(parts[1]);
+
+                for (int i = start; i <= end; i++)
+                {
+                    values.Add(i);
+                }
+            }
+            else
+            {
+                values.Add(int.Parse(field));
+            }
+
+            if (values.Any(v => v < minValue || v > maxValue))
+            {
+                throw new FormatException($"Invalid CRON expression: the value '{field}' is out of the range {minValue}-{maxValue}.");
+            }
+
+            return values.Where(v => v >= minValue && v <= maxValue).ToList();
+        }
+        public bool Contains(int value)
+        {
+            return _values.Contains(value);
+        }
+        public List<int> GetValues()
+        {
+            return _values;
+        }
+    }
+}

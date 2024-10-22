18cbd64a596f9876b4e969bc09dc64c27f087c2a
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-22 14:28:43 +0200

    feat: Add SchedulerHelper method - ReplacePlaceholders(string template, Enrollments enrollment)

--- mini-ITS.SchedulerService/SchedulerHelper.cs
+++ mini-ITS.SchedulerService/SchedulerHelper.cs
@@ -1,6 +1,20 @@
-﻿namespace mini_ITS.SchedulerService
+﻿using mini_ITS.Core.Models;
+
+namespace mini_ITS.SchedulerService
 {
     public class SchedulerHelper
     {
+        public static string ReplacePlaceholders(string template, Enrollments enrollment)
+        {
+            var properties = typeof(Enrollments).GetProperties();
+            foreach (var property in properties)
+            {
+                var placeholder = "{" + property.Name + "}";
+                var value = property.GetValue(enrollment)?.ToString() ?? string.Empty;
+                template = template.Replace(placeholder, value);
+            }
+
+            return template;
+        }
     }
 }

57d09623ce70621b6a5836e445a983b19a7bf475
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-22 18:20:37 +0200

    feat: Add SchedulerHelper method - BuildRecipientsList(SchedulerOptions taskConfig, UsersDto createUser, IEnumerable<UsersDto> departmentUsers, IEnumerable<UsersDto> departmentManagers, IEnumerable<UsersDto> admins, NotificationType type)

--- mini-ITS.SchedulerService/SchedulerHelper.cs
+++ mini-ITS.SchedulerService/SchedulerHelper.cs
@@ -1,11 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using mini_ITS.Core.Dto;
 using mini_ITS.Core.Models;
+using mini_ITS.SchedulerService.Options;
 
 namespace mini_ITS.SchedulerService
 {
     public class SchedulerHelper
     {
+        public enum NotificationType { Email, SMS }
+        public static bool IsPhoneValid(string phone) { return Regex.IsMatch(phone, @"^\d+$")}
+
         public static string ReplacePlaceholders(string template, Enrollments enrollment)
         {
             var properties = typeof(Enrollments).GetProperties();
@@ -51,6 +58,50 @@ namespace mini_ITS.SchedulerService
             }
 
             return messages;
+        }
+        public static List<UsersDto> BuildRecipientsList(
+            SchedulerOptions taskConfig,
+            UsersDto createUser,
+            IEnumerable<UsersDto> departmentUsers,
+            IEnumerable<UsersDto> departmentManagers,
+            IEnumerable<UsersDto> admins,
+            NotificationType type)
+        {
+            var recipients = new List<UsersDto>();
+
+            Action<IEnumerable<UsersDto>> addRecipients = users =>
+            {
+                if (type == NotificationType.Email && taskConfig.InfoByMail)
+                {
+                    recipients.AddRange(users.Where(u => !string.IsNullOrEmpty(u.Email)));
+                }
+                else if (type == NotificationType.SMS && taskConfig.InfoBySMS)
+                {
+                    recipients.AddRange(users.Where(u => !string.IsNullOrEmpty(u.Phone) && IsPhoneValid(u.Phone)));
+                }
+            };
+
+            if (taskConfig.InfoToCreateUser && createUser != null)
+            {
+                addRecipients(new List<UsersDto> { createUser });
+            }
+
+            if (taskConfig.InfoToDepartmentUsers && departmentUsers != null)
+            {
+                addRecipients(departmentUsers);
+            }
+
+            if (taskConfig.InfoToDepartmentManagers && departmentManagers != null)
+            {
+                addRecipients(departmentManagers);
+            }
+
+            if (taskConfig.InfoToAdmins && admins != null)
+            {
+                addRecipients(admins);
+            }
+
+            return recipients.Distinct().ToList();
         }
     }
 }

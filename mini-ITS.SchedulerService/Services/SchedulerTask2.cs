aa2faa0d5e76fb211c3cb782284c2469880c9ad1
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-24 21:25:10 +0200

    feat: Add SchedulerTask2 class

---
+++ mini-ITS.SchedulerService/Services/SchedulerTask2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using mini_ITS.SchedulerService.Options;
+
+namespace mini_ITS.SchedulerService.Services
+{
+    public class SchedulerTask2
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public SchedulerTask2(
+            IOptionsMonitor<SchedulerOptionsConfig> configMonitor,
+            ILogger<SchedulerTask2> logger,
+            IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+}

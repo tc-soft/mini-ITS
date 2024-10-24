0db9992f9975f9251c08c98db3fa55d0e8c0f91d
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-24 21:49:33 +0200

    feat: Add SchedulerTask3 class

---
+++ mini-ITS.SchedulerService/Services/SchedulerTask3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using mini_ITS.SchedulerService.Options;
+
+namespace mini_ITS.SchedulerService.Services
+{
+    public class SchedulerTask3
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public SchedulerTask3(
+            IOptionsMonitor<SchedulerOptionsConfig> configMonitor,
+            ILogger<SchedulerTask3> logger,
+            IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+    }
+}

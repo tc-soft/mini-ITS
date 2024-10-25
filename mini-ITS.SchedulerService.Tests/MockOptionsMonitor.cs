e034a1304dc03960ab031106b0718aba4f24ad6c
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-25 20:50:18 +0200

    feat: Add MockOptionsMonitor class for providing mock configuration in Nunit

---
+++ mini-ITS.SchedulerService.Tests/MockOptionsMonitor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+
+namespace mini_ITS.SchedulerService.Tests
+{
+    public class MockOptionsMonitor<T>(T currentValue) : IOptionsMonitor<T> where T : class, new()
+    {
+        private readonly T _currentValue = currentValue;
+        public T CurrentValue => _currentValue;
+        public T Get(string? name) => _currentValue;
+
+        public IDisposable? OnChange(Action<T, string> listener)
+        {
+            return null;
+        }
+    }
+}

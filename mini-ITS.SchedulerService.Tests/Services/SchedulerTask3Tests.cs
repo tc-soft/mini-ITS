bfcc0e27512403d506fd75dfa104193405389b29
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-27 14:14:23 +0100

    feat: Add SchedulerTask3Tests method - ValidScheduleTest(string schedule)

--- mini-ITS.SchedulerService.Tests/Services/SchedulerTask3Tests.cs
+++ mini-ITS.SchedulerService.Tests/Services/SchedulerTask3Tests.cs
@@ -1,9 +1,31 @@
 ﻿using NUnit.Framework;
+using mini_ITS.SchedulerService.Options;
+using mini_ITS.SchedulerService.Services;
 
 namespace mini_ITS.SchedulerService.Tests.Services
 {
     [TestFixture]
     public class SchedulerTask3Tests
     {
+        [TestCaseSource(typeof(SchedulerTaskTestsData), nameof(SchedulerTaskTestsData.ValidCronScheduleTestCases))]
+        public void ValidScheduleTest(string schedule)
+        {
+            var scheduleOptionsConfig = new SchedulerOptionsConfig
+            {
+                ["SchedulerTask3"] = new SchedulerOptions
+                {
+                    Active = true,
+                    Schedule = schedule
+                }
+            };
+
+            var optionsMonitor = new MockOptionsMonitor<SchedulerOptionsConfig>(scheduleOptionsConfig);
+            var serviceProvider = new MockServiceProvider();
+            var logger = new MockLogger<SchedulerTask3>();
+
+            var task = new SchedulerTask3(optionsMonitor, logger, serviceProvider);
+
+            Assert.That(task, Is.Not.Null, "SchedulerTask3 should be initialized correctly.");
+        }
     }
 }

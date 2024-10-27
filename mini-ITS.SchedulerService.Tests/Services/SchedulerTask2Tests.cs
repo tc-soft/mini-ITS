9d95a811e6117ac0a5ab9b872f8213b4788b36bd
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-27 13:11:47 +0100

    feat: Add SchedulerTask2Tests method - ValidScheduleTest(string schedule)

--- mini-ITS.SchedulerService.Tests/Services/SchedulerTask2Tests.cs
+++ mini-ITS.SchedulerService.Tests/Services/SchedulerTask2Tests.cs
@@ -1,9 +1,31 @@
 ﻿using NUnit.Framework;
+using mini_ITS.SchedulerService.Options;
+using mini_ITS.SchedulerService.Services;
 
 namespace mini_ITS.SchedulerService.Tests.Services
 {
     [TestFixture]
     public class SchedulerTask2Tests
     {
+        [TestCaseSource(typeof(SchedulerTaskTestsData), nameof(SchedulerTaskTestsData.ValidCronScheduleTestCases))]
+        public void ValidScheduleTest(string schedule)
+        {
+            var scheduleOptionsConfig = new SchedulerOptionsConfig
+            {
+                ["SchedulerTask2"] = new SchedulerOptions
+                {
+                    Active = true,
+                    Schedule = schedule
+                }
+            };
+
+            var optionsMonitor = new MockOptionsMonitor<SchedulerOptionsConfig>(scheduleOptionsConfig);
+            var serviceProvider = new MockServiceProvider();
+            var logger = new MockLogger<SchedulerTask2>();
+
+            var task = new SchedulerTask2(optionsMonitor, logger, serviceProvider);
+
+            Assert.That(task, Is.Not.Null, "SchedulerTask2 should be initialized correctly.");
+        }
     }
 }

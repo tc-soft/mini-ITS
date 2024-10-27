af7d0a3435c9d9dc324bda5c218c3ac6685d8495
Tadeusz Ciszewski <office@tc-soft.pl>
2024-10-27 14:19:28 +0100

    feat: Add SchedulerTask3Tests method - InvalidScheduleTest(string schedule)

--- mini-ITS.SchedulerService.Tests/Services/SchedulerTask3Tests.cs
+++ mini-ITS.SchedulerService.Tests/Services/SchedulerTask3Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using mini_ITS.SchedulerService.Options;
 using mini_ITS.SchedulerService.Services;
 
@@ -26,6 +27,25 @@ namespace mini_ITS.SchedulerService.Tests.Services
             var task = new SchedulerTask3(optionsMonitor, logger, serviceProvider);
 
             Assert.That(task, Is.Not.Null, "SchedulerTask3 should be initialized correctly.");
+        }
+        [TestCaseSource(typeof(SchedulerTaskTestsData), nameof(SchedulerTaskTestsData.InvalidCronScheduleTestCases))]
+        public void InvalidScheduleTest(string schedule)
+        {
+            var scheduleOptionsConfig = new SchedulerOptionsConfig
+            {
+                ["SchedulerTask3"] = new SchedulerOptions
+                {
+                    Active = true,
+                    Schedule = schedule
+                }
+            };
+
+            var optionsMonitor = new MockOptionsMonitor<SchedulerOptionsConfig>(scheduleOptionsConfig);
+            var serviceProvider = new MockServiceProvider();
+            var logger = new MockLogger<SchedulerTask3>();
+
+            Assert.Throws<FormatException>(() => new SchedulerTask3(optionsMonitor, logger, serviceProvider),
+               "ScheduleTask3 should throw FormatException for an invalid schedule.");
         }
     }
 }
